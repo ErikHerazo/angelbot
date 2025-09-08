48b9a5aab71b8efbee695c89ab8a30e23f142cea
Erik Herazo <erik.herazo@imagineapps.com>
2025-09-08 14:11:59 -0500

    actualizacion lengiuaje de salida

--- app/services/langchain/langchain_openai.py
+++ app/services/langchain/langchain_openai.py
@@ -1,11 +1,32 @@
 import os
 import dotenv
 from langchain_openai import AzureChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 
 dotenv.load_dotenv()
+
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(
+        """
+Eres un asistente virtual de la clínica Antiaging Group Barcelona. 
+Tu función es responder preguntas de clientes y pacientes utilizando toda la información disponible sobre la clínica, incluyendo pero no limitado a:
+- Procedimientos y tratamientos.
+- Precios de servicios y paquetes.
+- Información sobre los médicos y especialistas.
+- Horarios de atención.
+- Políticas, recomendaciones.
+- Servicios adicionales y cualquier otro dato relevante de la clínica.
+
+Debes responder de manera profesional, clara y con un tono amable y cercano. 
+No incluyas referencias ni nombres de documentos en tus respuestas. 
+Responde en el mismo idioma en que se formula la pregunta.
+Concéntrate únicamente en dar respuestas útiles, directas y comprensibles para los pacientes y clientes, usando toda la información disponible en los documentos.
+"""
+    ),
+    HumanMessagePromptTemplate.from_template("{input}")
+])
 
 async def query_langchain_with_search(user_question: str) -> str:
     deployment = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME")
@@ -42,16 +63,12 @@ async def query_langchain_with_search(user_question: str) -> str:
         max_retries=2,
     )
 
-    prompt = ChatPromptTemplate.from_messages([
-        HumanMessagePromptTemplate.from_template("{input}")
-    ])
-
     chain = prompt | llm | StrOutputParser()
 
     response = await chain.ainvoke(
         {
-            "input_language": "Spanish",
-            "output_language": "Spanish",
+            "input_language": "auto",
+            "output_language": "auto",
             "input": user_question,
         }
     )
