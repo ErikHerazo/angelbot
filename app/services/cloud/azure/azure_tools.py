6bcab0572613636617b95909602dab9773c10d30
Erik Herazo <erik.herazo@imagineapps.com>
2025-10-09 21:20:33 -0500

    se descomenta la funcion de validar personal, porque se habia subido comentada

--- app/services/cloud/azure/azure_tools.py
+++ app/services/cloud/azure/azure_tools.py
@@ -10,11 +10,45 @@ def get_current_time_spain() -> datetime:
     madrid_tz = ZoneInfo("Europe/Madrid")
     return datetime.now(madrid_tz)
 
+def is_customer_service_available(input: str = ""):
+    """
+    Indica si el personal de servicio al cliente está disponible actualmente en la clinica.
+    
+    Retorna un string JSON con:
+      - available: True/False
+      - message: texto explicativo que el modelo puede usar.
+    """
+    now = get_current_time_spain()
+    dia_semana = now.weekday()  # Lunes=0, Domingo=6
 
-# # Obtener hora actual de España
-# def get_current_time_spain() -> datetime:
-#     madrid_tz = ZoneInfo("Europe/Madrid")
-#     return datetime.now(madrid_tz)
+    # Lista de festivos en España
+    es_holidays = holidays.Spain(years=now.year)
+
+    available = False
+    message = "El servicio de atención al cliente no está disponible en este momento."
+
+    # Horario de lunes a viernes
+    if 0 <= dia_semana <= 4:
+        if time(8,0) <= now.time() <= time(12,0) or time(14,0) <= now.time() <= time(18,0):
+            available = True
+            message = "El servicio de atención al cliente está disponible actualmente."
+
+    # Horario sábado
+    elif dia_semana == 5:
+        if time(8,0) <= now.time() <= time(12,0):
+            available = True
+            message = "El servicio de atención al cliente está disponible actualmente."
+
+    # Domingo o festivo
+    if dia_semana == 6 or now.date() in es_holidays:
+        available = False
+        message = "El servicio de atención al cliente no está disponible en este momento."
+
+    return json.dumps({
+        "available": available,
+        "message": message
+    })
+
 
 # def is_customer_service_available(input: str = ""):
 #     """
@@ -24,52 +58,11 @@ def get_current_time_spain() -> datetime:
 #       - available: True/False
 #       - message: texto explicativo que el modelo puede usar.
 #     """
-#     now = get_current_time_spain()
-#     dia_semana = now.weekday()  # Lunes=0, Domingo=6
-
-#     # Lista de festivos en España
-#     es_holidays = holidays.Spain(years=now.year)
-
-#     available = False
-#     message = "El servicio de atención al cliente no está disponible en este momento."
-
-#     # Horario de lunes a viernes
-#     if 0 <= dia_semana <= 4:
-#         if time(8,0) <= now.time() <= time(12,0) or time(14,0) <= now.time() <= time(18,0):
-#             available = True
-#             message = "El servicio de atención al cliente está disponible actualmente."
-
-#     # Horario sábado
-#     elif dia_semana == 5:
-#         if time(8,0) <= now.time() <= time(12,0):
-#             available = True
-#             message = "El servicio de atención al cliente está disponible actualmente."
-
-#     # Domingo o festivo
-#     if dia_semana == 6 or now.date() in es_holidays:
-#         available = False
-#         message = "El servicio de atención al cliente no está disponible en este momento."
-
+#     # return test available=True
 #     return json.dumps({
-#         "available": available,
-#         "message": message
+#         "available": False,
+#         "message": "El servicio de atención al cliente no está disponible actualmente."
 #     })
-
-
-def is_customer_service_available(input: str = ""):
-    """
-    Indica si el personal de servicio al cliente está disponible actualmente en la clinica.
-    
-    Retorna un string JSON con:
-      - available: True/False
-      - message: texto explicativo que el modelo puede usar.
-    """
-    # return test available=True
-    return json.dumps({
-        "available": False,
-        "message": "El servicio de atención al cliente no está disponible actualmente."
-    })
-
 
 
 def ensure_users_table():
@@ -141,57 +134,57 @@ def save_user(name: str, email: str):
 
 
 tools = [
-        {
-            "type": "function",
-            "function": {
-                "name": "is_customer_service_available",
-                "description": "Comprueba si el servicio de atención al cliente está disponible actualmente en España. "
-                                "Utilízala cuando el usuario pregunte si puede ser atendido por un asesor, "
-                                "si hay soporte disponible, o si el horario de atención está activo. "
-                                "Devuelve True si el servicio está disponible en este momento, de lo contrario False.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "input": {
-                            "type": "string",
-                            "description": (
-                                "Texto opcional proporcionado por el usuario. "
-                                "Puede incluir su consulta o contexto, aunque no es necesario "
-                                "para determinar la disponibilidad del servicio."
-                            ),
-                        },
+    {
+        "type": "function",
+        "function": {
+            "name": "is_customer_service_available",
+            "description": "Comprueba si el servicio de atención al cliente está disponible actualmente en España. "
+                            "Utilízala cuando el usuario pregunte si puede ser atendido por un asesor, "
+                            "si hay soporte disponible, o si el horario de atención está activo. "
+                            "Devuelve True si el servicio está disponible en este momento, de lo contrario False.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "input": {
+                        "type": "string",
+                        "description": (
+                            "Texto opcional proporcionado por el usuario. "
+                            "Puede incluir su consulta o contexto, aunque no es necesario "
+                            "para determinar la disponibilidad del servicio."
+                        ),
                     },
-                    "required": ["input"],
                 },
-            }
-        },
-        {
-            "type": "function",
-            "function": {
-                "name": "save_user",
-                "description": "Guarda la información de un usuario en la base de datos. "
-                                "Utilízala cuando el usuario proporcione su nombre y correo electrónico "
-                                "para registrarse, dejar sus datos de contacto o continuar una solicitud con un asesor. "
-                                "La función crea la tabla 'users' si no existe y almacena el registro. "
-                                "Devuelve un objeto JSON con el estado de la operación ('status') y un mensaje descriptivo ('message').",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "name": {
-                            "type": "string",
-                            "description": (
-                                "Nombre del usuario. "
-                            ),
-                        },
-                        "email": {
-                            "type": "string",
-                            "description": (
-                                "Correo electronico del usuario, se usa para que un asesor de atencion al clinete contacte al usuario en horario disponible. "
-                            ),
-                        },
+                "required": ["input"],
+            },
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "save_user",
+            "description": "Guarda la información de un usuario en la base de datos. "
+                            "Utilízala cuando el usuario proporcione su nombre y correo electrónico "
+                            "para registrarse, dejar sus datos de contacto o continuar una solicitud con un asesor. "
+                            "La función crea la tabla 'users' si no existe y almacena el registro. "
+                            "Devuelve un objeto JSON con el estado de la operación ('status') y un mensaje descriptivo ('message').",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": (
+                            "Nombre del usuario. "
+                        ),
                     },
-                    "required": ["name", "email"],
+                    "email": {
+                        "type": "string",
+                        "description": (
+                            "Correo electronico del usuario, se usa para que un asesor de atencion al clinete contacte al usuario en horario disponible. "
+                        ),
+                    },
                 },
-            }
-        },
-    ]
+                "required": ["name", "email"],
+            },
+        }
+    },
+]
