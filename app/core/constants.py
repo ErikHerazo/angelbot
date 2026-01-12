5be09a882e897e2a03b43f6802e5023999254716
Erik Herazo <erik.herazo@imagineapps.com>
2026-01-12 13:01:04 -0500

    cmabio en saludo inicial y en primer consulta el valor

--- app/core/constants.py
+++ app/core/constants.py
@@ -57,6 +57,8 @@ Reglas:
 - IMPORTANTE: el nombre y correo electronico son obligatorios para el registro de usuarios, si el usuario no proporciona alguno de los dos, enviale un mensaje diciendo que ambos parametros son obligtorio para su registro.
 - Si el correo del usuario ya se encuetraba registrado en la base de datos, enviale un mensjae diciendole que ya existe un registro con ese email ejemplo: 'Ya tenemos un usuario registrado con ese correo, intenta con otro'.
 - Si te preguntan por los precios de tratamientos, cirugias o cualquier procedimiento relacionado a los que realiza la clinica, llama la funcion `procedures_and_treatments_price_list` para saber el rango o precio absoluto de ese servicio.
+- Si te preguntan por el precio de la consulta:
+La primera consulta es gratuita. En esta cita realizamos una valoración inicial, resolvemos todas tus dudas, explicamos las opciones disponibles y evaluamos si eres candidato al procedimiento, sin ningún compromiso.
 - IMPORTANTE: No calcules promedios ni ninguna operacion con los precios obtenidos, solo retornaselos al usuario y di que ese es el precio o rango aproximado, pero que todo esta sujeto a las condiciones de cada caso.
 - Ejemplos de precios:[
   {
--- app/services/chat/handlers/trigger_handler.py
+++ app/services/chat/handlers/trigger_handler.py
@@ -12,7 +12,9 @@ def handle_trigger():
                 "text": (
                     "👋 Hola.. soy Aesthea, el asistente virtual de "
                     "Antiaging Group Barcelona, tu clínica de medicina "
-                    "y cirugía estética. Nuestro objetivo es que te sientas mejor."
+                    "y cirugía estética. Nuestro objetivo es que te sientas mejor. "
+                    "\nPara ofrecerte un mejor servicio, y en caso de que tengamos que contactar contigo. "
+                    "Indícanos por favor tu nombre y email"
                 )
             }
         ]
