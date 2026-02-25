1291828dfabdcc438903ab1871d1a983bf50171c
Erik Herazo <erik.herazo@imagineapps.com>
2026-02-25 11:58:56 -0500

    implementacion de loggin con azure entra id

--- app/core/config.py
+++ app/core/config.py
@@ -9,4 +9,15 @@ class Settings:
     PRICE_LIST_CONTAINER = os.getenv("PRICE_LIST_CONTAINER")
     PRICE_LIST_BLOB = os.getenv("PRICE_LIST_BLOB")
 
+    # Azure AD
+    AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+    AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
+
 settings = Settings()
+
+# 🔐 Validación crítica al arrancar
+if not settings.AZURE_CLIENT_ID:
+    raise RuntimeError("AZURE_CLIENT_ID not configured")
+
+if not settings.AZURE_TENANT_ID:
+    raise RuntimeError("AZURE_TENANT_ID not configured")
