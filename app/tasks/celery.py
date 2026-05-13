3633469ef1ea6783a6e643f3362f511d4efe9ab7
Erik Herazo <erik.herazo@imagineapps.com>
2026-05-13 14:46:22 -0500

    Add celery worker deployment

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -10,7 +10,24 @@ app = Celery('angelbot',
 # Optional configuration, see the application user guide.
 app.conf.update(
     result_expires=3600,
+
+    broker_connection_retry_on_startup=True,
+    broker_connection_max_retries=None,
+
+    redis_socket_keepalive=True,
+    redis_retry_on_timeout=True,
+
+    broker_pool_limit=None,
 )
+
+# SSL for Azure Redis
+app.conf.broker_use_ssl = {
+    "ssl_cert_reqs": None
+}
+
+app.conf.redis_backend_use_ssl = {
+    "ssl_cert_reqs": None
+}
 
 if __name__ == '__main__':
     app.start()
